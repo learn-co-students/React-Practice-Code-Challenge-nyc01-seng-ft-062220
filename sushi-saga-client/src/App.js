595baef232eeee01589bac5d74226c51ae6572e5
Jeff-Adler <Jeffm.adler@gmail.com>
2020-08-29 13:06:13 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,54 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  constructor () {
+    super()
+
+    this.state = {
+      isLoaded : false,
+      plateStack : [],
+      budget : 50,
+      purchased : []
+    }
+  }
+
+  sushiRollClickHandler = (event) => {
+    const purchasedRoll = this.state.sushiRolls.find(sushiRoll => {
+      return (
+        sushiRoll.id === parseInt(event.target.id,10)
+      )
+    })
+    if (purchasedRoll.price <= this.state.budget) {
+      this.setState({budget: this.state.budget - purchasedRoll.price})
+      this.addPlate()
+      this.setState({purchased : this.state.purchased.concat(purchasedRoll.id)})
+    } else {
+    }
+
+  }
+
+  addPlate = () => {
+    this.setState({ plateStack : this.state.plateStack.concat("plate")})
+  }
+
+  componentDidMount () {
+    fetch(API)
+      .then(response => response.json())
+      .then(sushiRolls => {
+        this.setState({sushiRolls:sushiRolls,
+                      isLoaded : true})
+      })
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        {this.state.isLoaded === true ? <SushiContainer 
+                                          sushiRollClickHandler={this.sushiRollClickHandler} 
+                                          sushiRolls={this.state.sushiRolls} 
+                                          purchased={this.state.purchased}
+                                        /> : "Loading"}
+        <Table budget={this.state.budget} plateStack={this.state.plateStack}/>
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,53 @@
 import React, { Fragment } from 'react'
 
-const Sushi = (props) => {
-  return (
-    <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          false ?
-            null
-          :
-            <img src={/* Give me an image source! */ } width="100%" />
-        }
+class Sushi extends React.Component {
+
+  state = {
+    eaten : false
+  }
+
+  purchaseSushi = (event) => {
+    this.props.sushiRollClickHandler(event)
+  }
+
+  eatSushi = () => {
+    if (this.props.cannotPurchase !==  this.props.sushiRoll.id) {
+      this.setState({eaten:true})
+    }
+  }
+
+  checkIfPurchased = () => {
+    if (this.props.purchased.find(id => { 
+      return(
+        id === this.props.sushiRoll.id
+      ) 
+    })) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="sushi" >
+        <div className="plate">
+          { 
+            this.checkIfPurchased() ? null :       <img 
+                                                  src={this.props.sushiRoll.img_url} 
+                                                  width="100%" id={this.props.sushiRoll.id} 
+                                                  onClick={event => {this.purchaseSushi(event)}
+                                                          } 
+                                                />
+          }
+        </div>
+        <h4 className="sushi-details">
+          {this.props.sushiRoll.name} - ${this.props.sushiRoll.price}
+        </h4>
       </div>
-      <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
-      </h4>
-    </div>
-  )
+    )
+  }
 }
 
 export default Sushi
