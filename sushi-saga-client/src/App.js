a4d5ceb79de91f4c31c48baaa9965b866bbe6a18
yulia <juliana.ny2008@gmail.com>
2020-09-01 08:48:44 -0400

    1,2,3 deliverable

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -3,18 +3,41 @@ import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:8000/sushis"
 
 class App extends Component {
 
-  render() {
+  state={
+    money:100,
+    sushis: [],
+    fourSushis: []
+  }
+
+   clickHandler = () => { this.setState({
+                                        fourSushis: this.state.sushis.splice(1,4)})
+
+ }
+  render(){
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushis = {this.state}
+                        fourSushis = {this.state.fourSushis}
+                        clickHandler = {this.clickHandler}/>
+        <Table money = {this.state.money}/>
       </div>
     );
   }
-}
+  
 
+  componentDidMount(){
+
+    fetch(API)
+  .then(response => response.json())
+  .then(response => { this.setState({sushis: response,
+                                    fourSushis: response.splice(1, 4)
+                                    })  })
+                                    
+}
+}
 export default App;
+                                   
